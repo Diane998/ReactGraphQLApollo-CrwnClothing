bb20eaef386e46809cba5a446a06cac30b91543e
Diane998 <gutierrez.almiradyan@gmail.com>
2020-12-22 02:19:55 +0800

    Converted CollectionItem Component over to Apollo client local storage from react redux

--- client/src/containers/CollectionItemContainer.js
+++ client/src/containers/CollectionItemContainer.js
@@ -1,7 +1,24 @@
-import { connect } from 'react-redux';
-import { addItem } from '../redux/actions/cartActions';
+import React from 'react';
+
+import { useMutation, gql } from '@apollo/client';
+
 import CollectionItem from '../components/collections/CollectionItem';
 
-const CollectionItemContainer = connect(null, { addItem })(CollectionItem);
+const ADD_ITEM_TO_CART = gql`
+  mutation AddItemToCart($item: Item!) {
+    addItemToCart(item: $item) @client
+  }
+`;
+
+const CollectionItemContainer = props => {
+  const [addItemToCart] = useMutation(ADD_ITEM_TO_CART);
+
+  return (
+    <CollectionItem
+      {...props}
+      addItem={item => addItemToCart({ variables: { item } })}
+    />
+  );
+};
 
 export default CollectionItemContainer;
