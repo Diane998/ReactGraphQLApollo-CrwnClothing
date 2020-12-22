7aaecd189c293893887ff09c3238ded64fbcec46
Diane998 <gutierrez.almiradyan@gmail.com>
2020-12-22 18:41:51 +0800

    Converted CheckoutPageContainer Component over to Apollo client local storage from react redux

--- client/src/containers/CheckoutPageContainer.js
+++ client/src/containers/CheckoutPageContainer.js
@@ -1,20 +1,3 @@
-// import { connect } from 'react-redux';
-// import { createStructuredSelector } from 'reselect';
-// import {
-//   selectCartItems,
-//   selectCartTotal
-// } from '../redux/selectors/cartSelectors';
-// import CheckoutPage from '../components/pages/CheckoutPage';
-
-// const mapStateToProps = createStructuredSelector({
-//   cartItems: selectCartItems,
-//   total: selectCartTotal
-// });
-
-// const CheckoutPageContainer = connect(mapStateToProps)(CheckoutPage);
-
-// export default CheckoutPageContainer;
-
 import React from 'react';
 
 import { gql } from '@apollo/client';
